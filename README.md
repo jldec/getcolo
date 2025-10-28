a83166fede49418b0d4a646f4c285692b253ba85
jldec <jurgen@jldec.me>
2025-10-28 17:58:02 +0000

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# getcolo
+Tiny Cloudflare worker that returns its own instance-specific colo info as JSON.
+
+https://getcolo.jldec.me
--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		return Response.json({
+			region: request.cf.region,
+			colo: request.cf.colo,
+			country: request.cf.country,
+			city: request.cf.city
+		});
 	},
 };
