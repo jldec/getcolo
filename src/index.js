a3df389ac9867b6baddf6c56a0084ab00289a8cc
jldec <jurgen@jldec.me>
2025-11-07 13:48:41 +0000

    revert timestamp

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 export default {
   async fetch(request, env, ctx) {
-    const timestamp = request.url.endsWith('?ts') ? new Date().toISOString() : null
     return Response.json({
       colo: request.cf.colo,
       city: request.cf.city,
       region: request.cf.region,
       country: request.cf.country,
-      continent: request.cf.continent,
-      timestamp
+      continent: request.cf.continent
     })
   }
 }
